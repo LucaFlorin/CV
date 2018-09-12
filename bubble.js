56a17faa14ba1d101b17eab7c1dccd5230a6ea87
LucaFlorin <luca.florin.adrian@gmail.com>
2018-09-12 15:39:34 +0300

    progress4

--- bubble.js
+++ bubble.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $('.bubble1').click(function(){
-        $(this).addClass('pop');
+        $(this).addClass('side_pannel1');
     });
 
     $('.bubble2').click(function(){
-        $(this).addClass('pop2');
+        $(this).addClass('side_pannel2');
     });
 
 
@@ -12,21 +12,20 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
 
-        if( wScroll == 0 ){
-            $('.bubble1').removeClass('pop');
-            $('.bubble2').removeClass('pop2');
-            $('.pop_content').fadeOut('slow');
-            $('.pop_content2').fadeOut('slow');
-        }
 
-        if($(window).scrollTop() > 200) {
+        if($(window).scrollTop() > 100) {
             $('.bubble1').addClass('pop');
                 $('.pop_content').delay(1000).fadeIn('slow');
+                    $('.greeting_message').toggleClass('greeting_message_scroll');
             $('.bubble2').addClass('pop2');
                 $('.pop_content2').delay(1000).fadeIn('slow');
 
         }
-
-
     });
+
+    if($('.bubble1').hasClass('.side_pannel1') && $('.bubble2').hasClass('.side_pannel2')){
+        $('.nav_arrow').toggleClass('animation_arrow');
+    }
+
+
 });
