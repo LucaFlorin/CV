d6fa71856d6aa41bf35619077f029b48051c8003
LucaFlorin <luca.florin.adrian@gmail.com>
2018-09-12 16:44:00 +0300

    progress5

--- bubble.js
+++ bubble.js
@@ -8,24 +8,32 @@ $(document).ready(function(){
     });
 
 
+    $('.arrow_icon').click(function(){
+        $(this).toggleClass('arrow_active');
+        $('.left').toggleClass('left_active');
+        $('.middle').toggleClass('middle_active');
+        $('.right').toggleClass('right_active');
+        $('.right2').toggleClass('right_active');
+    });
+
+
+
 
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
 
 
-        if($(window).scrollTop() > 100) {
+        if($(window).scrollTop() > 30) {
             $('.bubble1').addClass('pop');
                 $('.pop_content').delay(1000).fadeIn('slow');
-                    $('.greeting_message').toggleClass('greeting_message_scroll');
+                  $('.title').hide();
             $('.bubble2').addClass('pop2');
                 $('.pop_content2').delay(1000).fadeIn('slow');
 
         }
     });
 
-    if($('.bubble1').hasClass('.side_pannel1') && $('.bubble2').hasClass('.side_pannel2')){
-        $('.nav_arrow').toggleClass('animation_arrow');
-    }
+
 
 
 });
